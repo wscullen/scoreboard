86abfe51e579ddcc18aa3345b53cbcf07c0d067a
Shaun Cullen <shaun@cullen.io>
2024-03-24 13:07:24 -0600

    Add vite-pwa-plugin to generate service worker and manifest for offline functionality.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   base: "/scoreboard/", // TODO: If you add a custom domain without a subpath, remove this line
 });
